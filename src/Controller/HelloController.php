f42781bd554e1a468a2686d68c3ac23a50392aef
JoaoDev <jpedroacady@gmail.com>
2024-11-12 16:41:40 -0300

    Incrementando o Annotation do Composer

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,7 +3,8 @@
     namespace App\Controller;
     
     use Symfony\Component\HttpFoundation\Response;
-    
+    use Symfony\Component\Routing\Annotation\Route;
+
     class HelloController 
     {
         /**
