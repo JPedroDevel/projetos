e6bd159e8420293a5c5752b5066ed40f9f2583d6
JoaoDev <jpedroacady@gmail.com>
2024-11-12 21:18:48 -0300

    Estruturando yaml,começando as rotas

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,19 +1,22 @@
 <?php
 
-    namespace App\Controller;
-    
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
+namespace App\Controller;
 
-    class HelloController 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+   #[Route('/')]
+    public function homepage()
     {
-        /**
-         * @Route("/path")
-         */
-        public function index(): Response
-        {
-           return new Response ("Hello Word");
-        }
+        return new Response('Sejam bem vindo <3');
+    }
+    
+    #[Route("/produtos/{itens}")]
+    public function show($itens)
+    {
+        return new Response('Bem vindo melhores produtos '.$itens);
     }
 
-?>
+}
